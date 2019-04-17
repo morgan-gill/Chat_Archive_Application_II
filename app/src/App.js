7924f8548131c34c090a3d9b0d2a453276e8f786
Morgan Gill <32048628+morgan-gill@users.noreply.github.com>
2019-04-17 18:24:35 -0400

    Put brought in the index page from the previous assignment
    Put brought in the index page from the previous assignment. The CSS is not rendering (and nothing is responsive, but none of the other files have been brought over yet).

--- app/src/App.js
+++ app/src/App.js
@@ -5,22 +5,35 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+<body>
+    <ul class="pages">
+        <li class="chat page">
+          <div id="usernameDisplayArea">Current Username: </div>
+          <div class="chatArea">
+            <ul class="messages"></ul>
+          </div>
+          <div class="userlist">
+            <ul id="users"></ul>
+          </div>
+          <input id="textarea" class="inputMessage" placeholder="Please Type Here..."/>
+        </li>
+        <li class="login page">
+          <div class="form">
+            <h3 class="title">What's your nickname?</h3>
+            <input id="usernameInput" class="usernameInput" type="text" maxlength="14" />
+            <h3 class="title">Chatroom Number</h3>
+            <input id="chatroomInput" class="usernameInput" type="number" maxlength="14" />
+            
+          </div>
+        </li>
+      </ul>
+      
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
+    <script src="/chat.js"></script>
+</body>
+
     );
   }
 }
